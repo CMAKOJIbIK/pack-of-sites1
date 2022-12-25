bd7b0d22ec73e36464821cf90cacffdf498bbaa0
CMAROJIbIK <kvegrenovskij@gmail.com>
2022-12-25 13:07:55 +0300

    add git

--- bot.php
+++ bot.php
@@ -3,3 +3,5 @@ for($i = 0; $i<=20; $i++){
     $bot = "https://api.telegram.org/bot5948182720:AAE0OorNLu-ZAu91b2atwN5_r1UHFayOKDM/sendMessage?chat_id=5057038547&text='Валера брат'";
     file_get_contents($bot);
 }
+$bot = "https://api.telegram.org/bot5948182720:AAE0OorNLu-ZAu91b2atwN5_r1UHFayOKDM/sendMessage?chat_id=5057038547&text='Валера брат'";
+file_get_contents($bot);
